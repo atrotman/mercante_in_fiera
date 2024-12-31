a0d35cfb852555ddc4ef1e08d1d2925b7e601d55
atrotman <64627298+atrotman@users.noreply.github.com>
2024-12-31 04:11:08 +0000

    some basic computer merchange functionality

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
   transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      tsconfig: 'tsconfig.json'
-    }]
+    '^.+\\.tsx?$': 'ts-jest'
   }
 }; 
